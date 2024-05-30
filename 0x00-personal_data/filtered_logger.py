d551a44583c4cbcf1dd2579105e4c01663c665cc
Mochatr <mohamedchatr8@gmail.com>
2024-05-30 22:18:03 +0000

    update

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,8 +5,13 @@ to obfuscate specific fields in a log message.
 """
 
 import logging
+import mysql.connector
+import os
 import re
 from typing import List
+
+
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
 def filter_datum(fields: List[str], redaction: str,
@@ -25,6 +30,57 @@ def filter_datum(fields: List[str], redaction: str,
     """
     return re.sub(rf'({"|".join(fields)})=.*?{separator}',
                   lambda m: f"{m.group(1)}={redaction}{separator}", message)
+
+
+def get_logger() -> logging.Logger:
+    """Returns a logging.Logger object"""
+    logger = logging.getLoger("user_data")
+    logger.propagate = False
+
+    handler = logging.StreamHandler()
+    handler.setLevel(logging.INFO)
+    handler.setFormatter(RedactingFormatter(PII_FIELDS))
+
+    logger.setLevel(logging.INFO)
+    logger.addHandler(handler)
+
+    return logger
+
+
+def get_db() -> mysql.connector.connection.MYSQLConnection:
+    """Returns a connector to the database"""
+    host = os.getenv('PERSONAL_DATA_DB_HOST') or 'localhost'
+    user = os.getenv('PERSONAL_DATA_DB_USERNAME') or 'root'
+    password = os.getenv('PERSONAL_DATA_DB_PASSWORD') or ''
+    database = os.getenv('PERSONAL_DATA_DB_NAME')
+
+    return mysql.connector.connect(
+        host=host,
+        port=3306,
+        user=user,
+        password=password,
+        database=database
+    )
+
+
+def main() -> None:
+    """
+    Acquire a database connection using get_db and retrieve all rows
+    in the users table and display each row under a filtered format
+    """
+    logger = get_logger()
+    connector = get_db()
+
+    cursor = connector.cursor()
+    cursor.execute('SELECT * FROM `users`;')
+
+    users = cursor.column_names
+    column_names = cursor.column_names
+
+    for user in users:
+        formatted_user = "".join(f"{attribute}={value}; " for
+                                 attribute, value in zip(column_names, user))
+        logger.info(formatted_user)
 
 
 class RedactingFormatter(logging.Formatter):
@@ -51,3 +107,7 @@ class RedactingFormatter(logging.Formatter):
         record.msg = filter_datum(self.fields, self.REDACTION,
                                   record.msg, self.SEPARATOR)
         return super(RedactingFormatter, self).format(record)
+
+
+if __name__ == "__main__":
+    main()
