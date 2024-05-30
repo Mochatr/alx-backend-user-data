c4634fdb4e7b910d09bf18c4e573f2e37f2ba910
Mochatr <mohamedchatr8@gmail.com>
2024-05-30 22:25:55 +0000

    Encrypting passwords

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Defines encrypt_password module"""
+
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """Returns a salted, hashed password"""
+    salt = bcrypt.gensalt()
+    return bcrypt.hashpw(password.encode(), salt)
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Validate the provided password matches
+    the hashed password.
+    """
+    return bcrypt.checkpw(password.encode(), hashed_password)
