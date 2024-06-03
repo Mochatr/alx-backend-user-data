2378f26ac8b53e93fdb53fd47c09e050ff96965b
Mochatr <mohamedchatr8@gmail.com>
2024-06-03 23:11:49 +0000

    Auth class

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+""" Auth module
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """Class to manage the API authentication"""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Public method that returns False"""
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """Public method that returns None"""
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Public method that returns None"""
+        return None
