908fad155d8de5c309451c58f5cf82fcc73e3a25
Mochatr <mohamedchatr8@gmail.com>
2024-06-07 18:00:54 +0000

    update user_object_from_credentials method

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -61,10 +61,10 @@ class BasicAuth(Auth):
             return None
         if user_pwd is None or not isinstance(user_pwd, str):
             return None
-        user = User.search({'email': user_email})
-        if not user:
+        users = User.search({'email': user_email})
+        if not users:
             return None
-        user = user[0]
+        user = users[0]
         if not user.is_valid_password(user_pwd):
             return None
         return user
